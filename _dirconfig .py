e03d0f73e050a2ab9371beedfee97f7678bf803b
gravy johnson <gravy@gravyjoneslock.uk>
2021-03-24 12:36:29 +0000

    Configured template dirconfig

---
+++ _dirconfig .py
@@ -0,0 +1,3 @@
+PYTHON_PATH = 'C:/Users/Edwar/anaconda3/envs/nuimgur/python.exe'
+SCRIPT_PATH = 'test.py'
+CMD = [PYTHON_PATH, SCRIPT_PATH, 'demo/static/input/[file_name]', 'demo/static/output/[file_name]'] 
